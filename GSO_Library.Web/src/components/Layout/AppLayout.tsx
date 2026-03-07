3d9f15a31e42deebf300f4c2c0098b8bc2842821
Josh Courtney <josh.c.courtney@gmail.com>
2026-03-07 09:46:37 -0500

    add github footer

--- GSO_Library.Web/src/components/Layout/AppLayout.tsx
+++ GSO_Library.Web/src/components/Layout/AppLayout.tsx
@@ -10,7 +10,7 @@ export default function AppLayout() {
         <Outlet />
       </Container>
       <footer className="bg-dark text-light text-center py-3 mt-auto">
-        <small>GSO Library &copy; {new Date().getFullYear()}</small>
+        <small>GSO Library &copy; {new Date().getFullYear()} &mdash; <a href="https://github.com/jcourtne/GSO_Library" target="_blank" rel="noreferrer" className="text-light">GitHub</a></small>
       </footer>
     </div>
   );
